cc90956ef7ce89ca9c73ae75019c04d6b8c99662
Marco Genova <vasckyla@hotmail.com>
2025-05-19 08:06:02 -0300

    vista posteo

--- src/pages/Resenas.jsx
+++ src/pages/Resenas.jsx
@@ -5,7 +5,7 @@ import '../components/posteos.css'
 export default function Resenas() {
   return (
     <main>
-      <PosteosDesdeSheet url="https://script.google.com/macros/s/AKfycbz3caW8VnrkH3Smr6J03ShVhZvGlnucBw5xxIsrgNK46eSRlWqb59SDBUmxc4YOcJAjzw/exec" base="Reseñas"/>
+      <PosteosDesdeSheet url="https://script.google.com/macros/s/AKfycbz3caW8VnrkH3Smr6J03ShVhZvGlnucBw5xxIsrgNK46eSRlWqb59SDBUmxc4YOcJAjzw/exec" base="blog"/>
     </main>
   )
 }
